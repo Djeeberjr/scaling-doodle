ba3a10c5e8713c6ba70074511dc90553351ce098
Djeeberjr <niklaskapelle@gmail.com>
2017-12-17 13:22:38 +0100

    finished Input

--- src/Input.java
+++ src/Input.java
@@ -1,10 +1,35 @@
-//Niklas
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Input {
-    public static KeyPress getInput(){
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static char getInput() throws IOException{
+        String s;
+        while(true){
+            s = sc.nextLine();
+
+            if(s.length() >= 1){
+                return s.charAt(0);
+            }
+        }
+
 
     }
 
-    public static Move getMove(Keypress input){
-
+    public static Move getMove(char input){
+        switch (input){
+            case 'w':
+                return Move.UP;
+            case 'a':
+                return Move.LEFT;
+            case 's':
+                return Move.DOWN;
+            case 'd':
+                return Move.RIGHT;
+            default:
+                return Move.INVALID;
+        }
     }
 }
